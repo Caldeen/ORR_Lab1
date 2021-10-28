3a625239cfbb229dfdfe066184a89c86ce04d212
Caldeen <jaro.tren6@gmail.com>
2021-10-28 14:16:57 +0200

    Initial commit

---
+++ src/ImplExample.java
@@ -0,0 +1,15 @@
+import java.rmi.RemoteException;
+
+// Implementing the remote interface
+public class ImplExample implements Hello {
+
+    // Implementing the interface method
+    public void printMsg() {
+        System.out.println("This is an example RMI program");
+    }
+
+    @Override
+    public int testReturn(int a, int b) throws RemoteException {
+        return a+b;
+    }
+} 
